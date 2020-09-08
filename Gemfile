76676f5c50338e520ae695f9c4db237b0228de6b
rudkovskij <a.rudkovskij@cft.ru>
2020-09-08 23:26:40 +0700

    bundle version change to 1.16.6 / add gem rake / add docker net to whitelisted_ips

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'sinatra', :require => false
 gem 'devise'
 gem 'rollbar'
 gem 'devise-bootstrap-views'
+gem 'rake'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.web_console.whitelisted_ips = '172.0.0.0/8'
 end
